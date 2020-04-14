2b23c07ff4745716da1767072499e6078add7bda
Retlev <rretlev@gmail.com>
2020-04-14 13:36:15 +0200

    project base

---
+++ src/main/java/com/mastery/aplsql/AplsqlApplication.java
@@ -0,0 +1,13 @@
+package com.mastery.aplsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplsqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AplsqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mastery/aplsql/AplsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mastery.aplsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplsqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
