b56e39f43776bbf6004463ec16d1c76221a80790
Retlev <rretlev@gmail.com>
2020-04-14 14:16:31 +0200

    Endpoints configured

--- src/main/java/com/mastery/aplsql/controller/QueryController.java
+++ src/main/java/com/mastery/aplsql/controller/QueryController.java
@@ -2,14 +2,31 @@ package com.mastery.aplsql.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import java.util.List;
 
 @Controller
 public class QueryController {
 
+    @PostMapping("/create")
+    public String createTable(){
+        return null;
+    }
+
     @GetMapping("/select")
     public List<List<String>> getResultOfQuery(){
+        return null;
+    }
+
+    @PostMapping("/insert")
+    public List<String> addRecord(){
+        return null;
+    }
+
+    @PutMapping("/update")
+    public List<String> updateRecord(){
         return null;
     }
 }
