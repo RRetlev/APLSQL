fd24eea391cc775f25576f32ae1130c1be454271
Adam Landfrasz <adam.landfrasz@gmail.com>
2020-04-15 13:37:09 +0200

    testing jenkins pipeline

--- src/test/java/com/mastery/aplsql/AplsqlApplicationTests.java
+++ src/test/java/com/mastery/aplsql/AplsqlApplicationTests.java
@@ -1,5 +1,6 @@
 package com.mastery.aplsql;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class AplsqlApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertEquals(1, 1);
     }
 
 }
