ea6c5a6ffb77c0ad5a0863cac44c9217163ff6a4
Retlev <rretlev@gmail.com>
2020-04-16 13:47:32 +0200

    Controller bugfix

--- src/main/java/com/mastery/aplsql/controller/QueryController.java
+++ src/main/java/com/mastery/aplsql/controller/QueryController.java
@@ -1,37 +1,36 @@
 package com.mastery.aplsql.controller;
 
+import com.mastery.aplsql.model.Query;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Queue;
 
-@Controller
+@RestController
 public class QueryController {
 
     @PostMapping("/create")
-    public String createTable(@RequestParam String query){
-        System.out.println(query);
+    public String createTable(@RequestBody Query query){
+        System.out.println(query.getQueryString());
         return null;
     }
 
     @GetMapping("/select")
-    public List<List<String>> getResultOfQuery(@RequestParam String query){
-        System.out.println(query);
+    public List<List<String>> getResultOfQuery(@RequestParam Query query){
+        System.out.println(query.getQueryString());
         return null;
     }
 
     @PostMapping("/insert")
-    public List<String> addRecord(@RequestParam String query){
-        System.out.println(query);
+    public List<String> addRecord(@RequestParam Query query){
+        System.out.println(query.getQueryString());
         return null;
     }
 
     @PutMapping("/update")
-    public List<String> updateRecord(@RequestParam String query){
-        System.out.println(query);
+    public List<String> updateRecord(@RequestParam Query query){
+        System.out.println(query.getQueryString());
         return null;
     }
 }
---
+++ src/main/java/com/mastery/aplsql/model/Query.java
@@ -0,0 +1,8 @@
+package com.mastery.aplsql.model;
+
+import lombok.Data;
+
+@Data
+public class Query {
+    private String queryString;
+}
