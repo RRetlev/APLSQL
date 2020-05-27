202d856cfcb3d240b8e257eac1bc15c468f971c9
Landfrasz Ádám <adam.landfrasz@gmail.com>
2020-05-27 16:39:27 +0200

    pipeline testing

--- src/main/java/com/mastery/aplsql/controller/QueryController.java
+++ src/main/java/com/mastery/aplsql/controller/QueryController.java
@@ -29,7 +29,7 @@ public class QueryController {
     Storage storage = new Storage();
     Storage temporaryStorage;
 
-    // using this to test if server is online
+    // using this to test if server is online while api testing in pipeline
     @GetMapping("/isworking")
     public Boolean isWorking() {
         return true;
